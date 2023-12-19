9d9b49337cb92c94cf81b76d826eed27e2fb349e
jaga-practice <jagadeeshjaga19@gmail.com>
2023-12-19 22:57:31 +0530

    changes made

--- Cpu_utilisation.sh
+++ Cpu_utilisation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cpu_threshold=01
-cpu_usage=$( top -bn2 | grep '%Cpu' | tail -1 | grep -P '(......|...)id,' 
+cpu_usage=$(top -bn2 | grep '%Cpu' | tail -1 | grep -P '(......|...)id,')
 message=""
 while IFS= read line;
 do
-  usage=$(echo $line | awk  '{print (100-$8)}'))
+  usage=$(echo $line | awk  '{print (100-$8)}')
     echo  "cpu usage : $usage"
     if [ $usage -ge $cpu_threshold ]
     then
